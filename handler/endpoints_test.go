0b6784afc594e975fe7fb02e515ebd4710ea1758
Muhammad Husein Jafari <mhuseinj@Muhammads-MacBook-Air.local>
2024-04-05 11:43:42 +0700

    create unit test for register, and add some additional unit test to increase coverage

--- handler/endpoints_test.go
+++ handler/endpoints_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/SawitProRecruitment/UserService/generated"
 	"github.com/SawitProRecruitment/UserService/repository"
 	"github.com/golang/mock/gomock"
@@ -232,6 +233,28 @@ func TestServer_Register(t *testing.T) {
 			},
 		},
 		{
+			name: "register with existing phone number",
+			args: args{
+				ctx: &ctxMock,
+			},
+			fields: fields{
+				Repository: mockRepo,
+			},
+			wantErr: false,
+			mock: func() {
+				req = httptest.NewRequest(http.MethodPost, "/register", strings.NewReader(
+					`{"phone":"+62812345678","password":"Password!1","fullname":"john due"}`))
+				req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+				rec = httptest.NewRecorder()
+				ctxMock = e.NewContext(req, rec)
+				mockRepo.EXPECT().CreateAccount(repository.Account{
+					FullName: "john due",
+					Phone:    "+62812345678",
+					Password: createHash("Password!1"),
+				}).Return(repository.Account{}, fmt.Errorf("duplicate constraint for phone"))
+			},
+		},
+		{
 			name: "register with unqualified password",
 			args: args{
 				ctx: &ctxMock,
@@ -242,7 +265,24 @@ func TestServer_Register(t *testing.T) {
 			wantErr: false,
 			mock: func() {
 				req = httptest.NewRequest(http.MethodPost, "/register", strings.NewReader(
-					`{"phone":"+62812345678","password":"password","fullname":"john due"}`))
+					`{"phone":"+62812345678","password":"pass","fullname":"john due"}`))
+				req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+				rec = httptest.NewRecorder()
+				ctxMock = e.NewContext(req, rec)
+			},
+		},
+		{
+			name: "register with unqualified phone and fullname",
+			args: args{
+				ctx: &ctxMock,
+			},
+			fields: fields{
+				Repository: mockRepo,
+			},
+			wantErr: false,
+			mock: func() {
+				req = httptest.NewRequest(http.MethodPost, "/register", strings.NewReader(
+					`{"phone":"0812345678","password":"password","fullname":"jo"}`))
 				req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 				rec = httptest.NewRecorder()
 				ctxMock = e.NewContext(req, rec)
@@ -263,11 +303,20 @@ func TestServer_Register(t *testing.T) {
 }
 
 func TestServer_UpdateProfile(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	var rec *httptest.ResponseRecorder
+	var req *http.Request
+	var ctxMock echo.Context
+	mockRepo := repository.NewMockRepositoryInterface(ctrl)
+	defer ctrl.Finish()
+	e := echo.New()
+	token, _ := createToken("token")
+
 	type fields struct {
 		Repository repository.RepositoryInterface
 	}
 	type args struct {
-		ctx    echo.Context
+		ctx    *echo.Context
 		params generated.UpdateProfileParams
 	}
 	tests := []struct {
@@ -275,102 +324,139 @@ func TestServer_UpdateProfile(t *testing.T) {
 		fields  fields
 		args    args
 		wantErr bool
+		mock    func()
 	}{
-		// TODO: Add test cases.
+		{
+			name: "update profile with phone and fullname",
+			args: args{
+				ctx: &ctxMock,
+				params: generated.UpdateProfileParams{
+					Authorization: token,
+				},
+			},
+			fields: fields{
+				Repository: mockRepo,
+			},
+			wantErr: false,
+			mock: func() {
+				req = httptest.NewRequest(http.MethodPut, "/profile", strings.NewReader(
+					`{"phone":"+62812345678","fullname":"john due"}`))
+				req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+				req.Header.Set(echo.HeaderAuthorization, token)
+				rec = httptest.NewRecorder()
+				ctxMock = e.NewContext(req, rec)
+				mockRepo.EXPECT().GetAccountByToken(ctxMock.Request().Context(), token).Return(repository.Account{Id: 1}, nil)
+				mockRepo.EXPECT().UpdateAccount(repository.Account{
+					Id:       1,
+					FullName: "john due",
+					Phone:    "+62812345678",
+				}).Return(repository.Account{}, nil)
+			},
+		},
+		{
+			name: "update profile with phone and duplicated",
+			args: args{
+				ctx: &ctxMock,
+				params: generated.UpdateProfileParams{
+					Authorization: token,
+				},
+			},
+			fields: fields{
+				Repository: mockRepo,
+			},
+			wantErr: false,
+			mock: func() {
+				req = httptest.NewRequest(http.MethodPut, "/profile", strings.NewReader(
+					`{"phone":"+62812345678"}`))
+				req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+				req.Header.Set(echo.HeaderAuthorization, token)
+				rec = httptest.NewRecorder()
+				ctxMock = e.NewContext(req, rec)
+				mockRepo.EXPECT().GetAccountByToken(ctxMock.Request().Context(), token).Return(repository.Account{Id: 1}, nil)
+				mockRepo.EXPECT().UpdateAccount(repository.Account{
+					Id:    1,
+					Phone: "+62812345678",
+				}).Return(repository.Account{}, fmt.Errorf("duplicate phone number"))
+			},
+		},
+		{
+			name: "update profile with fullname only",
+			args: args{
+				ctx: &ctxMock,
+				params: generated.UpdateProfileParams{
+					Authorization: token,
+				},
+			},
+			fields: fields{
+				Repository: mockRepo,
+			},
+			wantErr: false,
+			mock: func() {
+				req = httptest.NewRequest(http.MethodPut, "/profile", strings.NewReader(
+					`{"fullname":"john due updated"}`))
+				req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+				req.Header.Set(echo.HeaderAuthorization, token)
+				rec = httptest.NewRecorder()
+				ctxMock = e.NewContext(req, rec)
+				mockRepo.EXPECT().GetAccountByToken(ctxMock.Request().Context(), token).Return(repository.Account{Id: 1}, nil)
+				mockRepo.EXPECT().UpdateAccount(repository.Account{
+					Id:       1,
+					FullName: "john due updated",
+				}).Return(repository.Account{}, fmt.Errorf("duplicate phone number"))
+			},
+		},
+		{
+			name: "update profile with unexist profile",
+			args: args{
+				ctx: &ctxMock,
+				params: generated.UpdateProfileParams{
+					Authorization: token,
+				},
+			},
+			fields: fields{
+				Repository: mockRepo,
+			},
+			wantErr: false,
+			mock: func() {
+				req = httptest.NewRequest(http.MethodPut, "/profile", strings.NewReader(
+					`{"phone":"+62812345678"}`))
+				req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+				req.Header.Set(echo.HeaderAuthorization, token)
+				rec = httptest.NewRecorder()
+				ctxMock = e.NewContext(req, rec)
+				mockRepo.EXPECT().GetAccountByToken(ctxMock.Request().Context(), token).Return(repository.Account{}, fmt.Errorf("account not found"))
+			},
+		},
+		{
+			name: "update profile with unvalid token",
+			args: args{
+				ctx: &ctxMock,
+				params: generated.UpdateProfileParams{
+					Authorization: "unvalidtoken",
+				},
+			},
+			fields: fields{
+				Repository: mockRepo,
+			},
+			wantErr: false,
+			mock: func() {
+				req = httptest.NewRequest(http.MethodPut, "/profile", strings.NewReader(
+					`{"phone":"+62812345678"}`))
+				req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+				req.Header.Set(echo.HeaderAuthorization, "unvalidtoken")
+				rec = httptest.NewRecorder()
+				ctxMock = e.NewContext(req, rec)
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			tt.mock()
 			s := &Server{
 				Repository: tt.fields.Repository,
 			}
-			if err := s.UpdateProfile(tt.args.ctx, tt.args.params); (err != nil) != tt.wantErr {
+			if err := s.UpdateProfile(*tt.args.ctx, tt.args.params); (err != nil) != tt.wantErr {
 				t.Errorf("UpdateProfile() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func Test_createHash(t *testing.T) {
-	type args struct {
-		password string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := createHash(tt.args.password); got != tt.want {
-				t.Errorf("createHash() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_createToken(t *testing.T) {
-	type args struct {
-		username string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    string
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := createToken(tt.args.username)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("createToken() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("createToken() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_validateRegisterRequest(t *testing.T) {
-	type args struct {
-		request generated.RegisterRequest
-	}
-	tests := []struct {
-		name string
-		args args
-		want []string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := validateRegisterRequest(tt.args.request); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("validateRegisterRequest() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_verifyToken(t *testing.T) {
-	type args struct {
-		tokenString string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := verifyToken(tt.args.tokenString); (err != nil) != tt.wantErr {
-				t.Errorf("verifyToken() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
